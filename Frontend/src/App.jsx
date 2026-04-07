7667d8b4ddbb2b51b0df5b9e34b8b157d9a0f810
Laiba60 <laibasaeed026@gmail.com>
2026-04-07 18:14:26 +0500

    improve code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 };
 
